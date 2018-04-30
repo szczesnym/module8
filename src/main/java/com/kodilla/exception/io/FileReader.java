f40735698b4130c8e7830875068ddcb45990cc61
szczesnym <marcins@oerlemans-foods.nl>
2018-04-30 13:13:02 +0200

    Mod8 commit

--- src/main/java/com/kodilla/exception/io/FileReader.java
+++ src/main/java/com/kodilla/exception/io/FileReader.java
@@ -11,10 +11,10 @@ public class FileReader {
 
     public void readFile() {
        ClassLoader classLoader = getClass().getClassLoader();
-//        System.out.println(getClass().getClassLoader().findRe);
+//      System.out.println(getClass().getClassLoader().findRe);
         System.out.println(classLoader.getClass().getClassLoader());
-        //System.out.println(FileReader.class.getResource("/src/main/resources/names.txt"));
-        File file = new File("C:\\Users\\marcins\\IdeaProjects\\kodilla-course\\kodilla-exception\\src\\main\\resources\\names.txt");
+        File file = new File(classLoader.getResource("resources\\names.txt").getFile());
+       // File file = new File("C:\\Users\\marcins\\IdeaProjects\\kodilla-course\\kodilla-exception\\src\\main\\resources\\names.txt");
         System.out.println(file.getPath());
         Path path = Paths.get(file.getPath());
         try {
